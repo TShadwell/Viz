325c987748acd994574d45541e1b58ebdcf9f231
Harry Rickards <hrickards@gmail.com>
2012-07-08 10:13:23 +0100

    Quicker processing and sorting of A-Levels before hashings

--- ks5_parse.py
+++ ks5_parse.py
@@ -8,22 +8,33 @@ def isFloat(value):
   except:
     return False
 
-def a_level_hash(student):
-  return hashlib.md5(a_level_string(student)).hexdigest()
+def a_level_string(keys):
+  return "".join(key[4:] for key in keys if key.startswith("KS5") and not key.startswith("KS5_GAS") and key != "KS5_GENDER")
 
-def a_level_string(student):
-  return "".join(key[4:] for (key, value) in student.iteritems() if value != None and key.startswith("KS5") and not value.isdigit() and not isFloat(value) and key != "KS5_GENDER" and not key.startswith("KS5_GAS"))
+def gcse_string(keys):
+  return ",".join(key[4:] for key in keys if key.startswith("KS4"))
 
-def gcse_string(student):
-  return ",".join(key[4:] for (key, value) in student.iteritems() if value != None and key.startswith("KS4") and not value.isdigit() and not isFloat(value))
+def exam_string(student):
+  keys = [key for (key, value) in student.iteritems() if value != None and not str(value).isdigit() and not isFloat(value)]
+  if keys == []: return ""
+  keys.sort()
+
+  a_s = a_level_string(keys)
+
+  if a_s == "": return ""
+  
+  g_s = gcse_string(keys)
+  a_h = hashlib.md5(a_s).hexdigest()
+
+  return a_h + ":" + g_s
 
 connection = pymongo.Connection('localhost', 27017)
 
 db = connection.schools_db
 collection = db.ks5
 
-students = collection.find()
+students = collection.find(limit=50)
 
 f = open("hashes", "w")
-f.write("\n".join(a_level_hash(student) + ":" + gcse_string(student) for student in students if a_level_string(student) != ""))
+f.write("\n".join(filter(lambda x: x != "", (exam_string(student) for student in students if a_level_string(student) != ""))))
 f.write("\n")
