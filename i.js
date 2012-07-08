314c7261210e96e5881c3ba1dbeb1d64b6ebbc42
Thomas Shadwell <thomas.shadwell@googlemail.com>
2012-07-08 12:12:20 +0100

    more JS.

--- i.js
+++ i.js
@@ -16,20 +16,29 @@ init=function(){
 		this.y=y;
 	}
 	window.addEventListener('resize', resize, false);
-	var dot=function(x,y){
+	var dot=function(x,y, colour){
 		this.x=x;
 		this.y=y;
 		this.draw=function(){
 			c.arc(this.x, this.y, dotRad,  0, 2*Math.PI, true);
 		}
 	}
-	var line=function(point1, point2){
+	var line=function(point1, point2, colour){
 		this.pt1=point1;
 		this.pt2=point2;
+		this.colour=colour;
 		this.draw=function(){
-			moveTo(pt1.x, pt1.y);
-			lineTo(pt2.x, pt2.y);
+			c.beginPath();
+			c.strokeStyle=colour;
+			c.moveTo(this.pt1.x, this.pt1.y);
+			c.lineTo(this.pt2.x, this.pt2.y);
+			c.stroke();
 		}
 	}
+	//draw a line as a test
+	c.strokeStyle="red";
+	var x;
+	c.linewidth=100;
+	(x = new line(new point(5,5), new point(10,10), "red")).draw();
 
 }
